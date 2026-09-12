1201bc7690b31803028818468a947f1987fcfe4c
Admin <Admin@DESKTOP-AUJV498>
2026-09-12 12:44:43 +0530

    Documents and Tranning Models added

---
+++ HrmsCoreMvc/Models/Training/TrainingType.cs
@@ -0,0 +1,13 @@
+﻿namespace HrmsCoreMvc.Models
+{
+    public class TrainingType
+    {
+        public int TrainingTypeId { get; set; }
+
+        public string TrainingTypeName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Status { get; set; }
+    }
+}
