bacdbfe506028a3ddacc203143ec39be08ba6cfd
Khushiii19 <gawadekhushi19@gmail.com>
2026-09-12 21:33:14 +0530

    Added PayRoll model

---
+++ HrmsCoreMvc/Models/PayRoll/EarningType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HrmsCoreMvc.Models.PayRoll
+{
+    public class EarningType
+    {
+        [Key]
+        public int EarntypeId { get; set; }
+        public string EarningName { get; set; }
+
+        public List<Earning> Earnings { get; set; }
+    }
+}
