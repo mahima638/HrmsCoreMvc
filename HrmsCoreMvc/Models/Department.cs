fc6b87ab1818ae044473dcf377a591193a610fc7
Sanika <sanikamore792@gmail.com>
2026-09-12 09:29:17 +0530

    added department model

---
+++ HrmsCoreMvc/Models/Department.cs
@@ -0,0 +1,28 @@
+﻿namespace HrmsCoreMvc.Models
+{
+    public class Department
+    {
+        
+            [Key]
+            public int DepartmentId { get; set; }
+
+            public string Name { get; set; }
+
+            public int NoOfEmployee { get; set; }
+
+            public string Status { get; set; }
+
+            public DateTime CreatedAt { get; set; }
+
+            public string CreatedBy { get; set; }
+
+            public string ModifiedBy { get; set; }
+
+            public DateTime ModifiedAt { get; set; }
+
+            public List<User> userslists { get; set; }
+
+            public List<DepartmentLeaves> DepartmentLeavesList { get; set; }
+        
+    }
+}
