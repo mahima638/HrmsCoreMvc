23bae7e74cd75c6a01ab71223943fd79497d7481
mahima <mahimarp2004@gmail.com>
2026-09-13 12:30:55 +0530

    packages

---
+++ HrmsCoreMvc/Models/Register.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HrmsCoreMvc.Models
+{
+    public class Register
+    {
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
