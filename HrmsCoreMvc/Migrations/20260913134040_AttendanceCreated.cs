773520c2fcd5e3af267e5956250313482323e7db
mahima <mahimarp2004@gmail.com>
2026-09-13 22:05:52 +0530

    staged changes

---
+++ HrmsCoreMvc/Migrations/20260913134040_AttendanceCreated.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HrmsCoreMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AttendanceCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attendance",
+                columns: table => new
+                {
+                    AttendanceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CheckIn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CheckOut = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LunchIn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LunchOut = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    WorkingHours = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ProductionHours = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OvertimeHours = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    BreakHours = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Late = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attendance", x => x.AttendanceId);
+                    table.ForeignKey(
+                        name: "FK_Attendance_user_UserId",
+                        column: x => x.UserId,
+                        principalTable: "user",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MasterLeaveTypes",
+                columns: table => new
+                {
+                    LeaveTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LeaveType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MasterLeaveTypes", x => x.LeaveTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DepartmentLeaves",
+                columns: table => new
+                {
+                    DepartmentLeavesId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    LeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    MasterLeaveTypeLeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    LeavesCount = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartmentLeaves", x => x.DepartmentLeavesId);
+                    table.ForeignKey(
+                        name: "FK_DepartmentLeaves_MasterLeaveTypes_MasterLeaveTypeLeaveTypeId",
+                        column: x => x.MasterLeaveTypeLeaveTypeId,
+                        principalTable: "MasterLeaveTypes",
+                        principalColumn: "LeaveTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DepartmentLeaves_department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "department",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveRequests",
+                columns: table => new
+                {
+                    LeaveRequestId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    LeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    MasterLeaveTypeLeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NumberOfDays = table.Column<int>(type: "int", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApprovedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusHistory = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveRequests", x => x.LeaveRequestId);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequests_MasterLeaveTypes_MasterLeaveTypeLeaveTypeId",
+                        column: x => x.MasterLeaveTypeLeaveTypeId,
+                        principalTable: "MasterLeaveTypes",
+                        principalColumn: "LeaveTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequests_user_UserId",
+                        column: x => x.UserId,
+                        principalTable: "user",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaveBalances",
+                columns: table => new
+                {
+                    LeaveBalanceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    DepartmentLeavesId = table.Column<int>(type: "int", nullable: false),
+                    LeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    MasterLeaveTypeLeaveTypeId = table.Column<int>(type: "int", nullable: false),
+                    TotalLeaves = table.Column<int>(type: "int", nullable: false),
+                    UsedLeaves = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveBalances", x => x.LeaveBalanceId);
+                    table.ForeignKey(
+                        name: "FK_LeaveBalances_DepartmentLeaves_DepartmentLeavesId",
+                        column: x => x.DepartmentLeavesId,
+                        principalTable: "DepartmentLeaves",
+                        principalColumn: "DepartmentLeavesId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LeaveBalances_MasterLeaveTypes_MasterLeaveTypeLeaveTypeId",
+                        column: x => x.MasterLeaveTypeLeaveTypeId,
+                        principalTable: "MasterLeaveTypes",
+                        principalColumn: "LeaveTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LeaveBalances_user_UserId",
+                        column: x => x.UserId,
+                        principalTable: "user",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendance_UserId",
+                table: "Attendance",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmentLeaves_DepartmentId",
+                table: "DepartmentLeaves",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmentLeaves_MasterLeaveTypeLeaveTypeId",
+                table: "DepartmentLeaves",
+                column: "MasterLeaveTypeLeaveTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveBalances_DepartmentLeavesId",
+                table: "LeaveBalances",
+                column: "DepartmentLeavesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveBalances_MasterLeaveTypeLeaveTypeId",
+                table: "LeaveBalances",
+                column: "MasterLeaveTypeLeaveTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveBalances_UserId",
+                table: "LeaveBalances",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequests_MasterLeaveTypeLeaveTypeId",
+                table: "LeaveRequests",
+                column: "MasterLeaveTypeLeaveTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequests_UserId",
+                table: "LeaveRequests",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attendance");
+
+            migrationBuilder.DropTable(
+                name: "LeaveBalances");
+
+            migrationBuilder.DropTable(
+                name: "LeaveRequests");
+
+            migrationBuilder.DropTable(
+                name: "DepartmentLeaves");
+
+            migrationBuilder.DropTable(
+                name: "MasterLeaveTypes");
+        }
+    }
+}
