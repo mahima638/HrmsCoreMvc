cfdfeedf4c16ade42372496e56cbbecb620b01f6
mahima <mahimarp2004@gmail.com>
2026-09-13 14:48:01 +0530

    added key

--- HrmsCoreMvc/Models/Designation.cs
+++ HrmsCoreMvc/Models/Designation.cs
@@ -1,9 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HrmsCoreMvc.Models
 {
     public class Designation
     {
+
+        [Key]
         public int DesignationId { get; set; }
 
        
