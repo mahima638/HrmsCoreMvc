f566957201342a3d3f0388d65e14d58511fa5623
VIpin04345 <vipinyadav8950@gmail.com>
2026-09-14 15:46:23 +0530

    Create&Edit

---
+++ HrmsCoreMvc/Services/Promotions/PromotionService.cs
@@ -0,0 +1,27 @@
+﻿using HrmsCoreMvc.Data;
+using HrmsCoreMvc.Models.Promotion;
+using HrmsCoreMvc.Repositories.Promotions;
+using Microsoft.EntityFrameworkCore;
+
+namespace HrmsCoreMvc.Services.Promotions
+{
+    public class PromotionService : IPromotionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PromotionService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Promotion> GetPromotions()
+        {
+            return _context.promotion.Include(p => p.User).ToList();
+        }
+
+        public Promotion GetPromotionById(int promotionId)
+        {
+            return _context.promotion.Include(p => p.User).FirstOrDefault( p => p.PId == promotionId);
+        }
+    }
+}
