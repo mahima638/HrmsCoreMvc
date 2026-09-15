d10acd231867afcb864d8fc515420b4e7cd35c1d
VIpin04345 <vipinyadav8950@gmail.com>
2026-09-15 21:46:15 +0530

    updatesss

--- HrmsCoreMvc/Controllers/Promotion/PromotionController.cs
+++ HrmsCoreMvc/Controllers/Promotion/PromotionController.cs
@@ -13,31 +13,25 @@ namespace HrmsCoreMvc.Controllers
             _promotionRepository = promotionRepository;
         }
 
-        // Promotion List
         [HttpGet]
         public async Task<IActionResult> Index()
         {
             var promotions = await _promotionRepository.GetPromotionsAsync();
-
             return View(promotions);
         }
 
-        // Open Add Promotion Page
         [HttpGet]
         public async Task<IActionResult> AddPromotion()
         {
             var users = await _promotionRepository.GetUsersAsync();
-
             ViewBag.Users = users.Select(u => new
             {
                 UserId = u.UserId,
                 FullName = u.FirstName + " " + u.LastName
             }).ToList();
-
             return View();
         }
 
-        // Save New Promotion
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddPromotion(Promotion promotions)
@@ -45,49 +39,37 @@ namespace HrmsCoreMvc.Controllers
             if (ModelState.IsValid)
             {
                 await _promotionRepository.AddPromotionAsync(promotions);
-
                 TempData["SuccessMessage"] =
                     "Promotion added successfully.";
-
                 return RedirectToAction("Index");
             }
-
-            // Reload users if validation fails
             var users = await _promotionRepository.GetUsersAsync();
-
             ViewBag.Users = users.Select(u => new
             {
                 UserId = u.UserId,
                 FullName = u.FirstName + " " + u.LastName
             }).ToList();
-
             return View(promotions);
         }
 
-        // Open Edit Page
         [HttpGet]
         public async Task<IActionResult> Edit(int id)
         {
             var promotion =
                 await _promotionRepository.GetPromotionByIdAsync(id);
-
             if (promotion == null)
             {
                 return NotFound();
             }
-
             var users = await _promotionRepository.GetUsersAsync();
-
             ViewBag.Users = users.Select(u => new
             {
                 UserId = u.UserId,
                 FullName = u.FirstName + " " + u.LastName
             }).ToList();
-
             return View(promotion);
         }
 
-        // Update Promotion
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> UpdatePromotion(Promotion promotions)
@@ -95,21 +77,16 @@ namespace HrmsCoreMvc.Controllers
             if (ModelState.IsValid)
             {
                 await _promotionRepository.UpdatePromotionAsync(promotions);
-
                 TempData["SuccessMessage"] =
                     "Promotion updated successfully.";
-
                 return RedirectToAction("Index");
             }
-
             var users = await _promotionRepository.GetUsersAsync();
-
             ViewBag.Users = users.Select(u => new
             {
                 UserId = u.UserId,
                 FullName = u.FirstName + " " + u.LastName
             }).ToList();
-
             return View("Edit", promotions);
         }
 
@@ -120,26 +97,20 @@ namespace HrmsCoreMvc.Controllers
         {
             var promotion =
                 await _promotionRepository.GetPromotionByIdAsync(id);
-
             if (promotion == null)
             {
                 return NotFound();
             }
-
             await _promotionRepository.DeletePromotionAsync(id);
-
             TempData["SuccessMessage"] =
                 "Promotion deleted successfully.";
-
             return RedirectToAction("Index");
         }
-
         // Get Users as JSON
         [HttpGet]
         public async Task<IActionResult> GetUser()
         {
             var users = await _promotionRepository.GetUsersAsync();
-
             return Json(users);
         }
     }
